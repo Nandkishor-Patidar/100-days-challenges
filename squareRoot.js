b8480ddd4162218898a8c7d6c77f9e3013436994
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-02-17 07:51:48 +0530

    file upload

---
+++ squareRoot.js
@@ -0,0 +1,14 @@
+let a = 20;
+
+function squareRoot(num){
+    let result = a/2;
+    let x;
+    do{
+        x = result;
+        result = (x+(a/x))/2;
+    }while ((x - result)!=0){
+        return result;
+    }
+}
+
+console.log(squareRoot(a))
