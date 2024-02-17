bb0a4fca64e1565aa36e292fd4df267fd5a6c242
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-02-17 07:53:02 +0530

    "filem uplaod"

--- oddEven.js
+++ oddEven.js
@@ -1,16 +1,9 @@
-const arr =[1,2,3,4,5,6,7,8,9,10];
-const filteredFunction=(arr,callback)=>{
-  const filteredArr=[];
-  for (let i=0;i<arr.length;i++){
-    callback(arr[i]) ? filteredArr.push(arr[i]) : null;
-  }
-  return filteredArr;
+const arr =[112];
+function evenOdd(){
+if(arr%2==0){
+console.log("even");
+}else{
+console.log(odd);
 }
-const isOdd= (x) =>{
-  return x%2 !=0;
 }
-const isEven= (x) =>{
-  return x%2 ==0;
-}
-console.log(filteredFunction(arr,isOdd));
-console.log(filteredFunction(arr,isEven));
+evenOdd()
