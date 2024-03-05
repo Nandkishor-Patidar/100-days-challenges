02a7a128644f1701ed298fb65da91698c45cf6bc
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-03-05 20:44:24 +0530

    calculator

---
+++ simpleCalculator.js
@@ -0,0 +1,21 @@
+const opr = '%';
+const x = 30;
+const y = 25;
+
+let result ;
+if (opr == '+'){
+    result=x + y;
+}
+if(opr == '-'){
+    result = x-y;
+}
+if(opr == '*'){
+    result = x*y;
+}
+if(opr == '/'){
+    result = x/y;
+}
+if(opr == '%'){
+    result = x % y;
+}
+console.log(result)
