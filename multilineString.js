2a20de74151812b276ce2b105f2dce4c1661a97b
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-04-02 21:09:11 +0530

    Multiple Line

---
+++ multilineString.js
@@ -0,0 +1,15 @@
+let str =
+  "This is a book\n" + "that spans across multiple pages\n" + "in the book.";
+
+let str1 =
+     "This is a book\n\
+     that spans across multiple pages\n\
+     in the book";
+
+let str2 =
+      `This is a book
+       that spans across multiple pages
+       in the book`;
+console.log(str);
+console.log(str1);
+console.log(str2);
