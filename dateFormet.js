ac9a26096753140dd31a341b5c28ac355e0c4055
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-04-14 20:02:05 +0530

    Date

---
+++ dateFormet.js
@@ -0,0 +1,8 @@
+let currentDate = new Date();
+let day = currentDate.getDate();
+let month = currentDate.getMonth() + 1;
+let year = currentDate.getFullYear();
+console.log(year);
+console.log(month); 
+console.log(day);
+console.log(currentDate);
