a32f23b4864365990a0f16447bba7d8bd5fa58b2
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-04-16 08:11:34 +0530

    Compare Date

---
+++ compareDate.js
@@ -0,0 +1,10 @@
+const d1= new Date();
+const d2= new Date();
+const compare1 = d1<d2;
+const compare2 = d1>=d2;
+const compare3 = d1.getDate() === d2.getDate();
+const compare4 = d1.getDate() !== d2.getDate();
+console.log(compare4)
+console.log(compare1)
+console.log(compare2)
+console.log(compare3)
