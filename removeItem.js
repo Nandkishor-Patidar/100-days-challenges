394562ea66032fc18934fc6176eff36cd0a8355a
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-04-18 20:02:20 +0530

    Remove Item

---
+++ removeItem.js
@@ -0,0 +1,11 @@
+function removeItemFromArray(array, n) {
+    const index = array.indexOf(n);
+    if(index > -1) {
+        array.splice(index, 4);
+    }
+    return array;
+}
+
+const result = removeItemFromArray([1, 2, 3 , 4, 5], 2);
+
+console.log(result);
