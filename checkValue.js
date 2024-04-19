f6443f25ec0ca4965215abb67d8a3037a6cde0f7
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-04-19 17:35:47 +0530

    Check Value

---
+++ checkValue.js
@@ -0,0 +1,8 @@
+const array = ['you', 'will', 'learn', 'javascript'];
+
+const hasValue = array.includes('javascript');
+if(hasValue) {
+    console.log('Array contains a value.');
+} else {
+    console.log('Array does not contain a value.');
+}
