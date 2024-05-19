046d0fb96ebec6213205dc7faf583431416a58df
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-05-19 20:27:39 +0530

    Console

---
+++ writeConsole.js
@@ -0,0 +1,11 @@
+function sayName() {
+    return 'Hello Sohil';
+}
+console.log(sayName());
+const name = 'Yuvraj';
+console.log('Hello ' + name);
+let obj = {
+    name: 'John',
+    age: 22
+}
+console.log(obj);
