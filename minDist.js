ba53531e576fff20a0f5e92007f9aa5b16b8f229
Nandkishor-Patidar <nandupatidar14@gmail.com>
2024-06-05 20:26:34 +0530

    Minimum distance

---
+++ minDist.js
@@ -0,0 +1,19 @@
+function minDist(a, n, x, y){
+        let left = -1,right = -1,ans=n;
+        for(let i=0;i<n;i++){
+            if(a[i]==x){
+                left=i;
+            }
+            if(a[i]==y){
+                right=i;
+            }
+            if(left!= -1 && right!= -1){
+                ans = Math.min(ans,Math.abs(left - right));
+            }
+        }
+        return ans ==n? -1 : ans;
+    }
+let arr = [2,4,8,9,4,7,6,2,7,9,1,9,8];
+let n =arr.length;
+let x = 8,y=7;
+console.log(minDist(arr,n,x,y))
